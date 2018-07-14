6e381d784356df14f695993bc1af53961781de82
AndreyevVitaliy <37221679+AndreyevVitaliy@users.noreply.github.com>
2018-07-14 22:10:46 +0600

    v1

---
+++ main.py
@@ -0,0 +1,20 @@
+class Animals(object):
+
+    weight = 0
+    name = ""
+
+    def voice(self):
+        pass
+
+
+class Bird(Animals):
+    pass
+
+
+
+sheep = Animals()
+sheep.name = "123"
+
+print(sheep.name)
+
+# chicken = b
